80030324be2c915bddd9fa19a1e3f7d8485fd2a4
Rajan Poudel <raajan.works@gmail.com>
2025-02-07 19:32:14 +0545

    Add files via upload

---
+++ Largest.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+public class Largest {
+
+    public static void secondla(int arr[]) {
+        int size = arr.length;
+        if (size < 2) {
+            System.out.println("Invalid Input");
+            return;
+        } else {
+            Arrays.sort(arr);
+            System.out.println("Second largest element is: " + arr[size - 2]);
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {12, 35, 1, 10, 34, 1};
+        secondla(arr);
+    }
+}
