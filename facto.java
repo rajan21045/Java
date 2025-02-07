b8aa87348ebe4957c5d8721bdfa7ecf5b857872e
Rajan Poudel <raajan.works@gmail.com>
2025-02-07 19:31:26 +0545

    Add files via upload

---
+++ facto.java
@@ -0,0 +1,16 @@
+// Program to find the factorial of a number
+import java.util.Scanner;
+public class facto {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = sc.nextInt();
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact = fact * i;
+        }
+        System.out.println("Factorial of " + n + " is: " + fact);
+        sc.close();
+    }
+    
+}
