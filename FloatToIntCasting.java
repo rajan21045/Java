585916ba218c5ac3ab103785da8e64e83f7517ec
Rajan Poudel <raajan.works@gmail.com>
2025-04-18 16:38:56 +0545

    Add files via upload

---
+++ FloatToIntCasting.java
@@ -0,0 +1,8 @@
+public class FloatToIntCasting {
+    public static void main(String[] args) {
+        float floatValue = 19.99f;
+        int intValue = (int)floatValue;
+        System.out.println("Float Value : "+floatValue);
+        System.out.println("Integer Value (after explicit casting): "+intValue);
+    }
+}
---
+++ ImplicitTypeCasting.java
@@ -0,0 +1,8 @@
+public class ImplicitTypeCasting {
+    public static void main(String[] args) {
+        int intValue = 42;
+        double doubleValue = intValue;
+        System.out.println("Integer value: "+ intValue);
+        System.out.println("Double Value (after implicit conversion) : "+ doubleValue);
+    }
+}
