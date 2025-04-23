f57cc3e143dccc5d0ef135f6e9849ac9ae8a06eb
Rajan Poudel <raajan.works@gmail.com>
2025-04-23 15:31:49 +0545

    Update ContiMulti.java

--- ContiMulti.java
+++ ContiMulti.java
@@ -1,3 +1,4 @@
+//Write a Java program to print numbers from 1 to 100, skipping all multiples of 5 using the continue statement.
 public class ContiMulti {
         public static void main(String[] args) {
             for (int i = 1; i <= 100; i++) {
