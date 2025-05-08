a790d2a839c868dd9cf50d0f0ad41f0242f16446
Rajan Poudel <raajan.works@gmail.com>
2025-05-08 21:51:26 +0545

    Update ImplicitTypeCasting.java

--- ImplicitTypeCasting.java
+++ ImplicitTypeCasting.java
@@ -1,13 +1,15 @@
-/* Question:  
-
+/* 
+Question:  
 Write a Java program to demonstrate implicit type casting from `int` to `double`.
 */
 
 public class ImplicitTypeCasting {
+
     public static void main(String[] args) {
         int intValue = 42;
         double doubleValue = intValue;
         System.out.println("Integer value: "+ intValue);
-        System.out.println("Double Value (after implicit conversion) : "+ doubleValue);
-    }
+        System.out.println("Double Value (after implicit conversion): "+ doubleValue);
+    
+         }
 }
