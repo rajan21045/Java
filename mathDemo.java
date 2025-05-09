60dacf0075d228a81eb89efef2bae07f9b0509df
Rajan Poudel <raajan.works@gmail.com>
2025-05-09 11:48:59 +0545

    Add files via upload

---
+++ mathDemo.java
@@ -0,0 +1,21 @@
+import java.lang.Math;
+public class mathDemo {
+    public static void main(String[] args) {
+        int a =30;
+        int b=50;
+        int c =-1111111;
+        System.out.println("The Maximun Value is " +Math.max(a,b));
+        System.out.println("The Minimum Value is " + Math.min(a,b));
+        System.out.println("The Power of " + a + "Is " +Math.pow(a,4));
+        System.out.println("The Sqrt of "+a +" Is "+   Math.sqrt(a));
+        System.out.println( "The Absolute Value of " +c+ " Is "+  Math.abs(c));
+        System.out.println("The Round Up OF 4.4 Is "+Math.round(4.4) );
+        System.out.println("The Round Up of 4.6 Is "+Math.round(4.6));
+        System.out.println("The Ceiling Value of 2.3 Is "+Math.ceil(2.3));
+        System.out.println("The Flooring Value of 88.98 Is "+ Math.floor(88.98));
+        System.out.println("Example Of Showing Log() Is "+Math.log(2.2));
+        System.out.println("Example of Showing Sin() Is "+Math.sin(3.3));
+        System.out.println("Example Of Showing Cos() Is "+Math.cos(3.3  ));
+    }
+
+}
