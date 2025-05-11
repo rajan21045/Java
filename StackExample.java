b89bc92a130d7955b895261a2cb5a207f2ddbe6b
Rajan Poudel <raajan.works@gmail.com>
2025-05-11 12:28:51 +0545

    Update StackExample.java

--- StackExample.java
+++ StackExample.java
@@ -1,19 +1,25 @@
-import java.util.Stack;
+import java.util.Stack; // Importing Stack class from java.util package
+
 public class StackExample {
     public static void main(String[] args) {
-                        Stack<Integer>stk = new Stack<>();
-                        boolean result = stk.empty();
+        // Creating a Stack of Integer type
+        Stack<Integer> stk = new Stack<>();
 
-        System.out.println("Is The Stack Empty?: "+result);
+        // Checking if the stack is empty (should return true)
+        boolean result = stk.empty();
+        System.out.println("Is The Stack Empty?: " + result);
+
+        // Pushing elements onto the stack
         stk.push(78);
         stk.push(10111111);
         stk.push(2202);
         stk.push(22233455);
 
-        System.out.println("Elements In Stack: "+stk);
+        // Printing the elements in the stack
+        System.out.println("Elements In Stack: " + stk);
+
+        // Checking again if the stack is empty (should return false)
         result = stk.empty();
-
-        System.out.println("Is The Stack Empty?: "+result);
-
+        System.out.println("Is The Stack Empty?: " + result);
     }
 }
