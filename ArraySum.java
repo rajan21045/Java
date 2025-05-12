ee2a53d9e99bcb52f5c485b306c67b75afbf2b46
Rajan Poudel <raajan.works@gmail.com>
2025-05-12 17:00:59 +0545

    Update ArraySum.java

--- ArraySum.java
+++ ArraySum.java
@@ -1,9 +1,11 @@
-//Write a Java program to calculate the sum of all elements in an integer array.
+/* Write a Java program to calculate the 
+sum of all elements in an integer array. */
+
 public class ArraySum {
     public static void main(String[] args) {
+
         int[] arr = {10, 20, 30, 40, 50};
         int sum = 0;
-
 
         for (int i=0;i<arr.length; i++) {
             sum += arr[i];
