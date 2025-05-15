1a415f97ff5742294ffb81bc815d42aac885a730
Rajan Poudel <raajan.works@gmail.com>
2025-05-15 08:58:50 +0545

    Delete SkipMultiplesOfFive.java

--- SkipMultiplesOfFive.java
+++
@@ -1,8 +0,0 @@
-public class SkipMultiplesOfFive {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 100; i++) {
-            if (i % 5 == 0) continue;
-            System.out.print(i + " ");
-        }
-    }
-}
