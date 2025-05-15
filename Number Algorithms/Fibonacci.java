148d8a7325085430be7e9bb1797b64f791fc07d4
Rajan Poudel <raajan.works@gmail.com>
2025-05-15 12:04:06 +0545

    Create Fibonacci.java

---
+++ Number Algorithms/Fibonacci.java
@@ -0,0 +1,30 @@
+//program to print fibonacci series upto n terms
+import java.util.Scanner;
+public class Fibonacci {
+    public static int fibo(int n)
+    {
+
+        if(n==0)
+        return 0;
+        else if(n==1)
+        return 1;
+        else
+        return fibo(n-1)+fibo(n-2);
+
+    }
+
+
+    public static void main(String[] args)
+    {
+                 Scanner scanner = new Scanner(System.in);
+                System.out.print("ENTER THE NUMBER OF TERMS YOU WANT IN FIBONACCI SERIES: ");
+                int n = scanner.nextInt();
+                for(int i = 0; i<n; i++){
+                    System.out.println(fibo(i));
+                }
+
+                scanner.close();
+    }
+
+
+}
