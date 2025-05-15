333edf367e919ed6ca82a2146db32801ef96b145
Rajan Poudel <raajan.works@gmail.com>
2025-05-15 08:50:09 +0545

    Create Typecasting.java

---
+++ TypeCasting/Typecasting.java
@@ -0,0 +1,15 @@
+//SHOWING THE EXAMPLE OF TYPECASTING IN JAVA
+public class Typecasting {
+
+   public static void main(String[] args) {
+      int n = 10;
+      double d = n; // Automatic Typecasting
+      int a = 12;
+      byte b;
+      b = (byte) a; // Manual Typecasting
+            
+      System.out.println("Value of b: " + b);
+      System.out.println("Int value: " + n);
+      System.out.println("Double value: " + d);
+   }
+}
