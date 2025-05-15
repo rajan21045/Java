3f96ff9902eb639ed0c859f8cb53bc5ef2380ed7
Rajan Poudel <raajan.works@gmail.com>
2025-05-15 09:07:31 +0545

    Create StringReverse.java

---
+++ String/StringReverse.java
@@ -0,0 +1,25 @@
+/* 
+PROGRAM SHOWING THE EXAMPLE OF STRING REVERSE 
+
+*/
+
+
+import java.util.*;
+public class StringReverse{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a String ( in lower case without space ) : ");
+        String A=sc.nextLine();
+        String reversed = new StringBuilder(A).reverse().toString();
+        if(A.equals(reversed))
+        {
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+        sc.close();
+    }
+}
+
+
