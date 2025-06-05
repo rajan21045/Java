73dae6326012891cc0f0f9add688f9462fccc631
Rajan Poudel <raajan.works@gmail.com>
2025-06-05 08:28:16 +0545

    Update Student.java

--- Method & Constructor/Student.java
+++ Method & Constructor/Student.java
@@ -2,17 +2,23 @@
 public class Student{
     String name;
     int age;
+
                 public Student(String name, int age){
-                   this.name = name;
-                   this.age = age;
+                       this.name = name;
+                        this.age = age;
                 }   
+                
                 public void display(){
                     System.out.println("Name: " + name + " Age: " + age);
                 }
+
                 public static void main(String[] args) {
+
                     Student s1 = new Student("Rajan", 20);
                     Student s2 = new Student("Shishir", 22);
+
                     s1.display();
                     s2.display();
+
                 }
 }
