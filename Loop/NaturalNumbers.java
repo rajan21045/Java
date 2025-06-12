5eef093816d54634fc27ce9a8eb4dc164f958937
Rajan Poudel <raajan.works@gmail.com>
2025-06-12 17:54:11 +0545

    Update NaturalNumbers.java

--- Loop/NaturalNumbers.java
+++ Loop/NaturalNumbers.java
@@ -1,7 +1,9 @@
 public class NaturalNumbers {
     public static void main(String[] args) {
+        
         for (int i = 1; i <= 10; i++) {
             System.out.print(i + " ");
         }
+        
     }
 }
