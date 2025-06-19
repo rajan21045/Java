cd168e14eb74bce0ab3fbdaf5fb8dc1ee3ab4eec
Rajan Poudel <raajan.works@gmail.com>
2025-06-19 14:33:57 +0545

    Add files via upload

---
+++ Threading/Synchronization/synchronizationExample.java
@@ -0,0 +1,35 @@
+class First{
+    public void display(String msg){
+        System.out.println("["+msg);
+        try{
+            Thread.sleep(1000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        System.out.println("]");
+    }
+}
+class Second extends Thread{
+    String msg;
+    First fobj;
+    Second(First fp, String str){
+        fobj = fp;
+        msg = str;
+        start();
+    }
+    public void run(){
+        synchronized (fobj) //Synchronized Block
+        {
+         fobj.display(msg);
+        }
+    }
+}
+
+public class synchronizationExample {
+    public static void main(String[] args) {
+        First fnew = new First();
+        Second ss = new Second(fnew,"Welcome");
+        Second ss1 = new Second(fnew, "new");
+        Second ss2 = new Second(fnew,"Programmer");
+    }
+}
