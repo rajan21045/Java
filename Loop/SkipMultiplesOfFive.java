1ea237208f1c4a827e1b3a3095708827c84d32a4
Rajan Poudel <raajan.works@gmail.com>
2025-06-27 15:48:58 +0545

    Update SkipMultiplesOfFive.java

--- Loop/SkipMultiplesOfFive.java
+++ Loop/SkipMultiplesOfFive.java
@@ -2,7 +2,8 @@ public class SkipMultiplesOfFive {
     public static void main(String[] args) {
 
         for (int i = 1; i <= 100; i++) {
-            if (i % 5 == 0) 
+            
+                if (i % 5 == 0) 
                 continue;
             System.out.print(i + " ");
         }
