4ba7412876ea8d5a5c5f6f8bc89be115f34984b4
Rajan Poudel <raajan.works@gmail.com>
2026-04-18 21:52:31 +0545

    Update javaLearningPolymorphism.java

--- OOPS/javaLearningPolymorphism.java
+++ OOPS/javaLearningPolymorphism.java
@@ -1,5 +1,5 @@
 //Polymorphism means "many forms", and it occurs when we have many classes that are related to each other by inheritance.
-
+ 
 public class javaLearningPolymorphism {
     public static void main(String[] args) {
         Animal myAnimal = new Animal();  // Create a Animal object
