d2961d6a29bb355fe1639314c3dd5ac81714ba56
Rajan Poudel <raajan.works@gmail.com>
2026-04-27 20:59:52 +0545

    Add files via upload

---
+++ Conditional Operator/nestedIfElse.java
@@ -0,0 +1,17 @@
+class nestedIfElse {
+    public static void main(String[] args) {
+        int age = 20;
+        boolean isCitizen = true;
+
+        if (age >= 18) {
+            System.out.println("Old Enough To Vote");
+            if (isCitizen) {
+                System.out.println("And you are a citizen, so you can vote!");
+            } else {
+                System.out.println("But you must be a citizen to vote.");
+            }
+        } else {
+            System.out.println("Not old enough to vote.");
+        }
+    }
+}
