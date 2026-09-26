141a239726b1807fb3f482627216db9fec7f745b
Rajan Poudel <raajan.works@gmail.com>
2026-09-26 21:39:24 +0545

    Update Arstrongs.java

--- Number Algorithms/Arstrongs.java
+++ Number Algorithms/Arstrongs.java
@@ -2,6 +2,7 @@
 import java.util.Scanner;
 public class Arstrongs {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("ENTER THE NUMBER : ");  
         int n = scanner.nextInt();
